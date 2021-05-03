48ecf937775a1f3f8075903235f9d3be7deaffbb
Vichita Ok <vichita.ok@aphp.fr>
2021-05-03 17:40:53 +0200

    correction oubli // dans ftp

--- scripts/ncbi_fna.sh
+++ scripts/ncbi_fna.sh
@@ -19,7 +19,7 @@ table=/work_projet/mab_resistance/tables/ncbi_abscessus_ssp_abscessus.csv
 
 # Remove the "" in the .csv file
 #Collect and modify the FTP URLs of the table to point to the _genomic.fna.gz files (remove the first line (RefSeq FTP), separation in 2 blocks, then reconstruction with addition of the 2nd block)
-ftpR=$(sed 's/"//g' ${table} | cut -f 16 -d "," | grep -v 'RefSeq' | sed -r 's|(ftp:ftp.ncbi.nlm.nih.gov/genomes/all/.+/)(GCF_.+)|\1\2/\2_genomic.fna.gz|')
+ftpR=$(sed 's/"//g' ${table} | cut -f 16 -d "," | grep -v 'RefSeq' | sed -r 's|(ftp://ftp.ncbi.nlm.nih.gov/genomes/all/.+/)(GCF_.+)|\1\2/\2_genomic.fna.gz|')
 
 # Download the genomes using the FTP URLs created
 for ftp in ${ftpR}
