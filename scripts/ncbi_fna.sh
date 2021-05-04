ac0f2f7a2a3b731dc6412eb1a38fb7e0d3e39ddf
Vichita Ok <vichita.ok@aphp.fr>
2021-05-04 14:52:58 +0200

    lignes vides enlevees et test si fichier existe

--- scripts/ncbi_fna.sh
+++ scripts/ncbi_fna.sh
@@ -15,16 +15,24 @@ mkdir -p /work_projet/mab_resistance/GENOMES_NCBI
 mkdir -p /work_projet/mab_resistance/log
 
 # table exported from NCBI
-table=/work_projet/mab_resistance/tables/ncbi_abscessus_ssp_abscessus.csv
+table=/work_projet/mab_resistance/tables/prokaryotes.csv
 
 # Remove the "" in the .csv file
 #Collect and modify the FTP URLs of the table to point to the _genomic.fna.gz files (remove the first line (RefSeq FTP), separation in 2 blocks, then reconstruction with addition of the 2nd block)
-ftpR=$(sed 's/"//g' ${table} | cut -f 16 -d "," | grep -v 'RefSeq' | sed -r 's|(ftp://ftp.ncbi.nlm.nih.gov/genomes/all/.+/)(GCF_.+)|\1\2/\2_genomic.fna.gz|')
+ftpR=$(sed 's/"//g' ${table} | cut -f 16 -d "," | grep -Ev 'RefSeq|^$' | sed -r 's|(ftp://ftp.ncbi.nlm.nih.gov/genomes/all/.+/)(GCF_.+)|\1\2/\2_genomic.fna.gz|')
 
 # Download the genomes using the FTP URLs created
 for ftp in ${ftpR}
 do
-	wget ${ftp} -p /work_projet/mab_resistance/GENOMES_NCBI
+    echo ${ftp}
+    filename=$(cut -f 11 -d "/" ${ftp})
+    
+    if [[ -e /work_projet/mab_resistance/GENOMES_NCBI/${filename} ]]; then
+    echo "ERROR: Input file '${ftp}' already exists"
+    exit 1
+    fi
+    
+	wget ${ftp} -P /work_projet/mab_resistance/GENOMES_NCBI
 done
 
 
