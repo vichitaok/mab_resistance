47872fd057c97daa1ddf1a104267a47a588bdf98
Vichita Ok <vichita.ok@aphp.fr>
2021-05-04 15:30:13 +0200

    cut de  et non

--- scripts/ncbi_fna_test.sh
+++ scripts/ncbi_fna_test.sh
@@ -25,7 +25,7 @@ ftpR=$(sed 's/"//g' ${table} | cut -f 16 -d "," | grep -Ev 'RefSeq|^$' | sed -r 
 for ftp in ${ftpR}
 do
     echo ${ftp}
-    filename=$(cut -f 11 -d "/" ${ftp})
+    filename=$(cut -f 11 -d "/" ${ftpR})
     
     if [[ -e /work_projet/mab_resistance/GENOMES_NCBI_test/${filename} ]]; then
     echo "ERROR: Input file '${ftp}' already exists"
